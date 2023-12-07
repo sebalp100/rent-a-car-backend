e3425937b9608163529d6dce6b7bc6bf2c178613
sebalp100 <sebalp100@gmail.com>
2023-12-07 20:07:54 -0300

    Edit cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://rent-a-car-frontend-sebalp100.vercel.app/'
 
     resource(
       '*',
